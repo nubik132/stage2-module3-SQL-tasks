791c39d231366d69411c227e5c1430703a9fcad0
nubik132 <mister.nubik@list.ru>
2023-12-05 17:20:21 +0300

    feat: complete task 11

---
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -0,0 +1,7 @@
+UPDATE subject SET grade = 5 WHERE name like 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+
+UPDATE student SET groupnumber = 8 WHERE name like 'Tremaine Worvill';
+
+UPDATE payment SET amount = 500, student_id = 2 WHERE payment_date > '2021-01-01' AND type_id = 2;
+
+UPDATE mark SET mark = 2 WHERE student_id = 315;
