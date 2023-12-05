02bc66b190329b5b597fae206fcaefa30fc2da3a
nubik132 <mister.nubik@list.ru>
2023-12-05 17:15:22 +0300

    feat: complete task 10

---
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -0,0 +1,8 @@
+SELECT * FROM subject
+JOIN mark ON subject.id = mark.subject_id
+GROUP BY subject.name
+HAVING avg(mark.mark) > (SELECT avg(mark.mark) FROM mark);
+
+SELECT * FROM student
+JOIN payment ON student.id payment.student_id
+WHERE payment.amount < (SELECT avg(amount) FROM payment);
