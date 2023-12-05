f3ce407157395047a333241c2702bdf945938860
nubik132 <mister.nubik@list.ru>
2023-12-05 15:48:52 +0300

    feat: complete task 6

---
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -0,0 +1,15 @@
+SELECT * FROM payment
+WHERE type_id = 4;
+
+SELECT * FROM mark
+JOIN subject ON mark.subject_id = subject.id
+WHERE subject.name like 'Art';
+
+SELECT * FROM student
+JOIN payment ON payment.student_id = student.id
+WHERE type_id = 2;
+
+SELECT * FROM student
+JOIN mark ON student.id = mark.student_id
+JOIN subject ON mark.subject_id = subject.id
+WHERE subject.name like 'Math';
