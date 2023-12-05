b82ef7e82fb5b64e7a8d03444009c75b05fb7cbf
nubik132 <mister.nubik@list.ru>
2023-12-05 14:15:44 +0300

    feat: complete task 5

---
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -0,0 +1,20 @@
+SELECT * FROM payment
+WHERE amount >= 200;
+
+SELECT * FROM student
+WHERE dateadd(year, 20, birthday) < '20001010';
+
+SELECT * FROM student
+WHERE dateadd(year, 20, birthday) > '20001010' AND groupnumber = 10;
+
+SELECT * FROM student
+WHERE name like 'Mike' or (groupnumber >= 4 AND groupnumber <= 6);
+
+SELECT * payment
+WHERE dateadd(month, 8, payment_date) < '20001010';
+
+SELECT * FROM student
+WHERE name like 'A%';
+
+SELECT * FROM student
+WHERE (name like 'Roxi' and groupnumber = 4) or (name = 'Tallie' AND groupnumber = 9);
