1142dffc31dc90df7825bc3fb8ff3cb8cadd728a
nubik132 <mister.nubik@list.ru>
2023-12-05 15:54:09 +0300

    feat: complete task 7

---
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -0,0 +1,18 @@
+SELECT * FROM mark
+WHERE mark > 6
+ORDER BY mark DESC;
+
+SELECT * FROM payment
+WHERE amount < 300
+ORDER BY amount;
+
+SELECT * FROM paymenttype
+ORDER BY name;
+
+SELECT * FROM student
+ORDER BY name DESC;
+
+SELECT * FROM student
+JOIN payment ON student.id = payment.student_id
+WHERE amount > 1000
+ORDER BY student.birthday;
