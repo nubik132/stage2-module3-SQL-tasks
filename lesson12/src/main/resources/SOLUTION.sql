0bbd287ea067dccdc7ec3263709ac3d4d2065b0c
nubik132 <mister.nubik@list.ru>
2023-12-05 18:12:56 +0300

    refactor

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,6 +1,6 @@
 DELETE FROM student WHERE grade >= 4;
 
-DELETE FROM student WHERE id IN (SELECT student.id FROM student JOIN mark ON student.id = mark.student_id WHERE m.mark < 4);
+DELETE FROM student WHERE id IN (SELECT student.id FROM student JOIN mark ON student.id = mark.student_id WHERE mark < 4);
 
 DELETE FROM paymenttype WHERE name like 'Daily';
 
