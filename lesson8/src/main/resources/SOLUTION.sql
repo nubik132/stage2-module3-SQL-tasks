c1e53d97cafdf2687106ca27503dbd371bf6098f
nubik132 <mister.nubik@list.ru>
2023-12-05 16:02:46 +0300

    feat: complete task 8

---
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -0,0 +1,10 @@
+SELECT min(birthday) FROM student;
+
+SELECT min(payment_date) FROM payment;
+
+SELECT avg(mark) FROM mark
+JOIN subject ON mark.subject_id = subject.id
+WHERE subject.name like 'Math';
+
+SELECT min(amount) FROM payment
+WHERE type_id = 2;
